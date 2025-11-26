7c46e05536aea2d1bc0a374452597b0085c78a5f
unknown <nhalelkyizyi23@apec.edu.kz>
2025-11-26 14:10:01 +0500

    added

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import products
+from .views import products, basket_add
 app_name = 'products'       
 
 urlpatterns = [
     path('products/', products, name='index'),
+    path('basket/', basket_add, name='basket')
 ]
